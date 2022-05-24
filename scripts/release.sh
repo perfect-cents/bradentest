61e41c5c61a105d3b075cf546dcf9a46492ba100
Braden Pezeshki <braden.pezeshki@gmail.com>
2022-05-24 00:50:10 -0700

    asdf

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
 set -e
 
 VERSION_JSON=$(yarn version apply --json)
@@ -8,7 +10,7 @@ if [ -n "$VERSION_JSON" ]; then
   VERSION=$(node -e "console.log(($VERSION_JSON).newVersion)")
   PKG_NAME=$(node -e "console.log(($VERSION_JSON).ident)")
 
-  git add -u
+  git add package.json $dir/../.yarn/versions
   git commit -m "$PKG_NAME@$VERSION"
   git tag "$PKG_NAME@$VERSION"
 
